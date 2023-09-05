0f0abab9207f5613777c336e01b44176c92b960c
Gtker <git@gtker.com>
2023-09-05 16:28:21 +0200

    Add client/server defines

--- include/wow_srp/wow_srp.h
+++ include/wow_srp/wow_srp.h
@@ -3,36 +3,62 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 /**
  * Generator used by the server implementation.
  *
  * This must be provided to clients in order for them to use it.
  */
 #define WOW_SRP_GENERATOR 7
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define CLIENT_HEADER_LENGTH 6
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define WOW_SRP_SUCCESS 0
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define WOW_SRP_ERROR_NULL_POINTER 1
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define WOW_SRP_ERROR_UTF8 2
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define WOW_SRP_ERROR_NON_ASCII 3
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define WOW_SRP_ERROR_INVALID_PUBLIC_KEY 4
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 #define WOW_SRP_ERROR_PROOFS_DO_NOT_MATCH 5
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 typedef struct WowSrpClient WowSrpClient;
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 typedef struct WowSrpClientChallenge WowSrpClientChallenge;
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 typedef struct WowSrpClientUser WowSrpClientUser;
+#endif
 
+#if defined(WOW_SRP_SERVER)
 typedef struct WowSrpProof WowSrpProof;
+#endif
 
+#if defined(WOW_SRP_SERVER)
 typedef struct WowSrpServer WowSrpServer;
+#endif
 
 #if defined(WOW_SRP_TBC_HEADER)
 typedef struct WowSrpTbcHeaderCrypto WowSrpTbcHeaderCrypto;
@@ -50,7 +76,9 @@ typedef struct WowSrpVanillaHeaderCrypto WowSrpVanillaHeaderCrypto;
 typedef struct WowSrpVanillaProofSeed WowSrpVanillaProofSeed;
 #endif
 
+#if defined(WOW_SRP_SERVER)
 typedef struct WowSrpVerifier WowSrpVerifier;
+#endif
 
 #if defined(WOW_SRP_WRATH_HEADER)
 typedef struct WowSrpWrathClientCrypto WowSrpWrathClientCrypto;
@@ -70,36 +98,54 @@ extern "C" {
 
 extern const uint8_t WOW_SRP_LARGE_SAFE_PRIME_LITTLE_ENDIAN[32];
 
+#if defined(WOW_SRP_CLIENT)
 void wow_srp_client_free(struct WowSrpClient *client);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 const uint8_t *wow_srp_client_session_key(struct WowSrpClient *client);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 void wow_srp_client_calculate_reconnect_values(struct WowSrpClient *client,
                                                const uint8_t *server_challenge_data,
                                                uint8_t *out_client_challenge_data,
                                                uint8_t *out_client_proof,
                                                char *out_error);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 void wow_srp_client_challenge_free(struct WowSrpClientChallenge *client_challenge);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 const uint8_t *wow_srp_client_challenge_client_proof(struct WowSrpClientChallenge *client_challenge);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 const uint8_t *wow_srp_client_challenge_client_public_key(struct WowSrpClientChallenge *client_challenge);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 struct WowSrpClient *wow_srp_client_challenge_verify_server_proof(struct WowSrpClientChallenge *client_challenge,
                                                                   const uint8_t *server_proof,
                                                                   char *out_error);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 struct WowSrpClientUser *wow_srp_client_user_from_username_and_password(const char *username,
                                                                         const char *password,
                                                                         char *out_error);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 struct WowSrpClientChallenge *wow_srp_client_user_into_challenge(struct WowSrpClientUser *client_user,
                                                                  uint8_t generator,
                                                                  const uint8_t *large_safe_prime,
                                                                  const uint8_t *server_public_key,
                                                                  const uint8_t *salt,
                                                                  char *out_error);
+#endif
 
 #if defined(WOW_SRP_TBC_HEADER)
 void wow_srp_tbc_proof_seed_free(struct WowSrpTbcProofSeed *seed);
@@ -263,45 +309,73 @@ void wow_srp_wrath_client_crypto_decrypt(struct WowSrpWrathClientCrypto *header,
                                          char *out_error);
 #endif
 
+#if defined(WOW_SRP_SERVER)
 void wow_srp_proof_free(struct WowSrpProof *proof);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_proof_server_public_key(const struct WowSrpProof *proof);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_proof_salt(const struct WowSrpProof *proof);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpServer *wow_srp_proof_into_server(struct WowSrpProof *proof,
                                                const uint8_t *client_public_key,
                                                const uint8_t *client_proof,
                                                uint8_t *out_server_proof,
                                                char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 void wow_srp_server_free(struct WowSrpServer *server);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_server_session_key(const struct WowSrpServer *server);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_server_reconnect_challenge_data(const struct WowSrpServer *server);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 bool wow_srp_server_verify_reconnection_attempt(struct WowSrpServer *server,
                                                 const uint8_t *client_data,
                                                 const uint8_t *client_proof,
                                                 char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpVerifier *wow_srp_verifier_from_username_and_password(const char *username,
                                                                    const char *password,
                                                                    char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpVerifier *wow_srp_verifier_from_database_values(const char *username,
                                                              const uint8_t *password_verifier,
                                                              const uint8_t *salt,
                                                              char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpProof *wow_srp_verifier_into_proof(struct WowSrpVerifier *verifier);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_verifier_salt(const struct WowSrpVerifier *verifier);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_verifier_password_verifier(const struct WowSrpVerifier *verifier);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 void wow_srp_verifier_free(struct WowSrpVerifier *verifier);
+#endif
 
 #ifdef __cplusplus
 } // extern "C"
--- include/wow_srp/wow_srp.hpp
+++ include/wow_srp/wow_srp.hpp
@@ -4,34 +4,60 @@
 #include <ostream>
 #include <new>
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 /// Generator used by the server implementation.
 ///
 /// This must be provided to clients in order for them to use it.
 constexpr static const uint8_t WOW_SRP_GENERATOR = 7;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const uint8_t CLIENT_HEADER_LENGTH = 6;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const char WOW_SRP_SUCCESS = 0;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const char WOW_SRP_ERROR_NULL_POINTER = 1;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const char WOW_SRP_ERROR_UTF8 = 2;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const char WOW_SRP_ERROR_NON_ASCII = 3;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const char WOW_SRP_ERROR_INVALID_PUBLIC_KEY = 4;
+#endif
 
+#if (defined(WOW_SRP_CLIENT) || defined(WOW_SRP_SERVER))
 constexpr static const char WOW_SRP_ERROR_PROOFS_DO_NOT_MATCH = 5;
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 struct WowSrpClient;
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 struct WowSrpClientChallenge;
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 struct WowSrpClientUser;
+#endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpProof;
+#endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpServer;
+#endif
 
 #if defined(WOW_SRP_TBC_HEADER)
 struct WowSrpTbcHeaderCrypto;
@@ -49,7 +75,9 @@ struct WowSrpVanillaHeaderCrypto;
 struct WowSrpVanillaProofSeed;
 #endif
 
+#if defined(WOW_SRP_SERVER)
 struct WowSrpVerifier;
+#endif
 
 #if defined(WOW_SRP_WRATH_HEADER)
 struct WowSrpWrathClientCrypto;
@@ -67,36 +95,54 @@ extern "C" {
 
 extern const uint8_t WOW_SRP_LARGE_SAFE_PRIME_LITTLE_ENDIAN[32];
 
+#if defined(WOW_SRP_CLIENT)
 void wow_srp_client_free(WowSrpClient *client);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 const uint8_t *wow_srp_client_session_key(WowSrpClient *client);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 void wow_srp_client_calculate_reconnect_values(WowSrpClient *client,
                                                const uint8_t *server_challenge_data,
                                                uint8_t *out_client_challenge_data,
                                                uint8_t *out_client_proof,
                                                char *out_error);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 void wow_srp_client_challenge_free(WowSrpClientChallenge *client_challenge);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 const uint8_t *wow_srp_client_challenge_client_proof(WowSrpClientChallenge *client_challenge);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 const uint8_t *wow_srp_client_challenge_client_public_key(WowSrpClientChallenge *client_challenge);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 WowSrpClient *wow_srp_client_challenge_verify_server_proof(WowSrpClientChallenge *client_challenge,
                                                            const uint8_t *server_proof,
                                                            char *out_error);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 WowSrpClientUser *wow_srp_client_user_from_username_and_password(const char *username,
                                                                  const char *password,
                                                                  char *out_error);
+#endif
 
+#if defined(WOW_SRP_CLIENT)
 WowSrpClientChallenge *wow_srp_client_user_into_challenge(WowSrpClientUser *client_user,
                                                           uint8_t generator,
                                                           const uint8_t *large_safe_prime,
                                                           const uint8_t *server_public_key,
                                                           const uint8_t *salt,
                                                           char *out_error);
+#endif
 
 #if defined(WOW_SRP_TBC_HEADER)
 void wow_srp_tbc_proof_seed_free(WowSrpTbcProofSeed *seed);
@@ -260,44 +306,72 @@ void wow_srp_wrath_client_crypto_decrypt(WowSrpWrathClientCrypto *header,
                                          char *out_error);
 #endif
 
+#if defined(WOW_SRP_SERVER)
 void wow_srp_proof_free(WowSrpProof *proof);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_proof_server_public_key(const WowSrpProof *proof);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_proof_salt(const WowSrpProof *proof);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 WowSrpServer *wow_srp_proof_into_server(WowSrpProof *proof,
                                         const uint8_t *client_public_key,
                                         const uint8_t *client_proof,
                                         uint8_t *out_server_proof,
                                         char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 void wow_srp_server_free(WowSrpServer *server);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_server_session_key(const WowSrpServer *server);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_server_reconnect_challenge_data(const WowSrpServer *server);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 bool wow_srp_server_verify_reconnection_attempt(WowSrpServer *server,
                                                 const uint8_t *client_data,
                                                 const uint8_t *client_proof,
                                                 char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 WowSrpVerifier *wow_srp_verifier_from_username_and_password(const char *username,
                                                             const char *password,
                                                             char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 WowSrpVerifier *wow_srp_verifier_from_database_values(const char *username,
                                                       const uint8_t *password_verifier,
                                                       const uint8_t *salt,
                                                       char *out_error);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 WowSrpProof *wow_srp_verifier_into_proof(WowSrpVerifier *verifier);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_verifier_salt(const WowSrpVerifier *verifier);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 const uint8_t *wow_srp_verifier_password_verifier(const WowSrpVerifier *verifier);
+#endif
 
+#if defined(WOW_SRP_SERVER)
 void wow_srp_verifier_free(WowSrpVerifier *verifier);
+#endif
 
 } // extern "C"
